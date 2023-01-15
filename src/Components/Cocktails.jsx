40a726eecb27819a57720434c30a70a0e513cd54
Mhd Ali Houria <alihoria123456@gmail.com>
2023-01-15 11:01:09 +0300

    minor changes

--- src/Components/Cocktails.jsx
+++ src/Components/Cocktails.jsx
@@ -2,6 +2,18 @@ import React from "react";
 import getIngredientsArr from "../utils/getIngredientsArr";
 
 export const Cocktails = ({details }) => {
+  /*
+  !Things to use to structure the component
+  1- strDrink => Drink name
+  2- strCategory => Category type
+  3- strAlcoholic => Is it alcoholic or not
+  4- strGlass => Glass type 
+  (2,3,4 are probably going to be on the same line)
+  5- strInstructions => instructions
+  6- strDrinkThumb => Thumbnail of drink
+  7- arrOfIngredients (my made array) => to show case the ingredients needed
+  
+  */
 
   const arrOfIngredients = getIngredientsArr(details)
 
